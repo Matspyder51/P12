14849c4904adf71b4defd76cb72af410b63eeb87
Mathis Cucci <mat.spyder@gmail.com>
2021-12-12 22:02:26 +0100

    Fix admin user creation

--- epicevents/core/admin.py
+++ epicevents/core/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from core.models import User, Client, Contract, Event
+from django import forms
 
 # Register your models here.
 
-admin.site.register([User, Client, Contract, Event])
+admin.site.register([Client, Contract, Event])
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username", "password", "first_name", "last_name", "email", "groups")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    form = UserForm
+    list_display = ("id", "username", "first_name", "last_name", "email")
